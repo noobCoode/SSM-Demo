f202933d0dff2cf831c0ce6223b84adf693f1a4e
1097374154@qq.com <1097374154@qq.com>
2018-04-10 10:28:59 +0800

    feat:SSM-Demo v1.0 start

--- src/main/Java/Controllers/StudentController.java
+++ src/main/Java/Controllers/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/Student")
+@RequestMapping("/api/Students")
 public class StudentController {
     @Autowired
     private IStudentService studentService;
